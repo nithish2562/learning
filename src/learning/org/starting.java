50a1428a358f705efa146d2fb1c7c9e216eb29d0
nithish2562 <nithishthanukrishnan@gmail.com>
2022-03-07 19:08:27 +0530

    done for b

--- src/learning/org/starting.java
+++ src/learning/org/starting.java
@@ -4,7 +4,8 @@ public class starting {
 	public static void main(String[] args) {
 		System.out.println("starting");
 		System.out.println("git");
-	
+		System.out.println("su=witching to b");
+	System.out.println("int was going to show the conflict i think so");
 	}
 	
 
