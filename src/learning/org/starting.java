96d66b095665017c1226af375bf2b4ef1eee5d62
nithish2562 <nithishthanukrishnan@gmail.com>
2022-03-07 19:06:28 +0530

    done

--- src/learning/org/starting.java
+++ src/learning/org/starting.java
@@ -4,7 +4,12 @@ public class starting {
 	public static void main(String[] args) {
 		System.out.println("starting");
 		System.out.println("git");
+		System.out.println("completed fetching");
+		System.out.println("starting");
+		System.out.println("git");
+		System.out.println("completed fetching");
 	
+
 	}
 	
 
